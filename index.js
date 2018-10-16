22c99dea68d26d9b36ef84a7ddf330202c58388e
Joe Mullen <mullenjoe13@gmail.com>
2018-10-16 23:36:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,4 +30,6 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 
-var theFunk = console.log(funkyFunction())
+var theFunk = funkyFunction() {
+  return function()
+}
