4c66515d12f9e71e889a91372f7c717b1bf24c19
Joe Mullen <mullenjoe13@gmail.com>
2018-10-16 23:28:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,6 +30,6 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 
 
-var theFunk = funkyFunction
-
-function funkyFunction()
+function theFunk() {
+  return funkyFunction
+}
